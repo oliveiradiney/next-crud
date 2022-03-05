d1323d7d02204a64d0134a1bb22dd56c4ef20437
oliveiradiney <dineyoliveira1999@gmail.com>
2022-03-05 13:34:56 -0300

    Componente Botao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout"
 import Tabela from '../components/Tabela';
 import Cliente from '../core/Cliente'
+import Botao from '../components/Botao'
 
 export default function Home() {
 
@@ -19,18 +20,24 @@ export default function Home() {
     console.log(`Excluir... ${cliente.nome}`);
   }
   return (
-    <div className={`
+    <div
+      className={`
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
       text-white
-    `}>
-        <Layout titulo="Cadastro Simples">
-          <Tabela 
-            clientes={clientes} 
-            clienteSelecionado={clienteSelecionado} 
-            clienteExcluido={clienteExcluido}
-          />
-        </Layout>
+    `}
+    >
+      <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end">
+          <Botao cor="green" className="mb-4">Novo cliente</Botao>
+        </div>
+
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
+      </Layout>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  content: [
+  purge: {
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+    ],
+    safelist: [
+      /^bg-/,
+      /^to-/,
+      /^from-/,
+    ]
+
+  },
+  
   theme: {
     extend: {},
   },
