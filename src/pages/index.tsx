bdff461c4ded90cca960f06d6d477144de5dca2d
oliveiradiney <dineyoliveira1999@gmail.com>
2022-07-27 00:49:45 -0300

    integrando cadastro com firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,36 @@ import Tabela from '../components/Tabela';
 import Cliente from '../core/Cliente'
 import Botao from '../components/Botao'
 import Formulario from "../components/Formulario";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import ClienteRepositorio from "../core/ClienteRepositorio";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
 
 export default function Home() {
+
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela') 
 
-  const clientes = [
-    new Cliente("Ana", 34, "1"),
-    new Cliente("Bia", 20, "2"),
-    new Cliente("Carlos", 25, "3"),
-    new Cliente("Pedro", 21, "4"),
-  ];
+  useEffect(obterTodos,[])
+
+  function obterTodos(){
+    repo.obterTodos()
+    .then(clientes => {
+        setClientes(clientes)
+        setVisivel('tabela')
+    })
+  }
 
   function clienteSelecionado(cliente: Cliente){
       setCliente(cliente)
       setVisivel('form')
   }
 
-  function clienteExcluido(cliente: Cliente) {
-    console.log(`Excluir... ${cliente.nome}`);
+  async function clienteExcluido(cliente: Cliente) {
+    await repo.excluir(cliente)
+    obterTodos()
   }
 
   function novoCliente(){
@@ -30,9 +40,9 @@ export default function Home() {
     setVisivel('form')
   }
 
-  function salvarCliente(cliente: Cliente){
-    console.log(cliente)
-    setVisivel('tabela')
+  async function salvarCliente(cliente: Cliente){
+    await repo.salvar(cliente)
+    obterTodos()
   }
   return (
     <div
